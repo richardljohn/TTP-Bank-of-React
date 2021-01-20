68439a0b40f272e4ce01b56ade5012f750324baa
richardljohn <richardljohn@gmail.com>
2021-01-19 23:44:44 -0500

    Couldn't finish App.js

--- bank-of-react/src/App.js
+++ bank-of-react/src/App.js
@@ -1,39 +1,17 @@
 import React, {Component} from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
-import Home from './components/Home';
-import UserProfile from './components/UserProfile';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Credit from './Components/Credits'
+import Debit from './Components/Debits';
+import Home from './Components/Home';
+import Login from './Components/Login';
+import UserProfile from './Components/UserProfile';
+import axios from 'axios';
+import './App.css';
 
 class App extends Component {
-
   constructor() {
     super();
-
-    this.state = {
-      accountBalance: 14568.27,
-      currentUser: {
-        userName: 'richardljohn',
-        memberSince: '01/04/20',
-      }
-    }
   }
-
-  render() {
-
-    const HomeComponent = () => (<Home accountBalance={this.state.accountBalance}/>);
-    const UserProfileComponent = () => (
-        <UserProfile userName={this.state.currentUser.userName} memberSince={this.state.currentUser.memberSince}  />
-    );
-
-    return (
-        <Router>
-          <div>
-            <Route exact path="/" render={HomeComponent}/>
-            <Route exact path="/userProfile" render={UserProfileComponent}/>
-          </div>
-        </Router>
-    );
-  }
-
 }
 
 export default App;
