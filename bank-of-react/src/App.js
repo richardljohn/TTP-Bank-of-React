1cf4a84d874097b90ac6425a288375a7a99c21b9
richardljohn <richardljohn@gmail.com>
2021-01-25 04:09:41 -0500

    Some more stuff

--- bank-of-react/src/App.js
+++ bank-of-react/src/App.js
@@ -12,7 +12,36 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
+      accountBalance: 0,
+      currentUser: {
+        userName: "richardljohn",
+        memberSince: "01/04/2021"
+      },
     }
+  }
+
+  mockLogIn = (logInInfo) => {
+    const newUser = {...this.state.currentUser}
+    newUser.userName=logInInfo.userName
+    this.setState({currentUser:newUser})
+  }
+  render() {
+    const HomeComponent = () => (<Home accountBalance={this.state.accountBalance}/>);
+    const UserProfileComponent=() => (
+      <UserProfile userName={this.state.currentUser.userName} memberSince={this.state.currentUser.memberSince} />
+    );
+    const LogInComponent = () => (<Login user={this.state.currentUser} mockLogIn= {this.mockLogin} {...this.props}/>)
+    return (
+      <Router>
+        <div>
+          <Route exact path="/" render={Home}/>
+          <Route exact path="/userProfile" render={UserProfile} />
+          <Route exact path="/login" render={Login} />
+          <Route exact path='/debits' component={Debit} />
+          <Route exact path='/credits' component={Credit} />
+        </div>
+      </Router>
+    );
   }
 }
 
